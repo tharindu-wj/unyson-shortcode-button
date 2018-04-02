d5ce48513bfd6422635131724fa170e7a4e2f87b
Tharindu <tharinduw@eight25media.com>
2018-04-02 19:02:23 +0530

    Commit two

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $cfg = array();
 
 $cfg['page_builder'] = array(
 	'title'       => __( 'E25 Button', 'fw' ),
-	'description' => __( 'Add a Button 123', 'fw' ),
+	'description' => __( 'Add a Button', 'fw' ),
 	'tab'         => __( 'E25 Elements', 'fw' ),
 	'popup_size' => 'medium'
 );
